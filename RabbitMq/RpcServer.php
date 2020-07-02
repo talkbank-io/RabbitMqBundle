64f7bd1191b4a7968a4dda45531154c4b970ca15
Pavel Loginov <ploginoff@gmail.com>
2020-07-02 08:38:20 +0300

    ack only after reply

--- RabbitMq/RpcServer.php
+++ RabbitMq/RpcServer.php
@@ -17,10 +17,10 @@ class RpcServer extends BaseConsumer
     public function processMessage(AMQPMessage $msg)
     {
         try {
-            $msg->delivery_info['channel']->basic_ack($msg->delivery_info['delivery_tag']);
             $result = call_user_func($this->callback, $msg);
             $result = call_user_func($this->serializer, $result);
             $this->sendReply($result, $msg->get('reply_to'), $msg->get('correlation_id'));
+            $msg->delivery_info['channel']->basic_ack($msg->delivery_info['delivery_tag']); // after reply
             $this->consumed++;
             $this->maybeStopConsumer();
         } catch (\Exception $e) {
